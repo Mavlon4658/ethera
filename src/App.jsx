56052ff9ff284b9411361130798ba52b4fd7610a
Mavlon4658 <mavlonbe4658>
2024-05-16 16:06:58 +0500

    project start

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hello word</h1>
-      
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique eaque doloremque sapiente dolorum nihil? Ratione labore voluptas adipisci autem quis sed laborum in aut iusto deserunt quasi eligendi nihil, iure quae iste ullam! Sapiente quas reiciendis, neque accusantium at, voluptatibus adipisci facere ipsum molestias fuga, aperiam quo minus! Optio dolores doloremque dolorem vero pariatur beatae suscipit, reprehenderit provident eos blanditiis voluptatem quae fugit saepe odit. Aut blanditiis ipsa rem quas doloribus, magnam voluptate nemo porro, repudiandae velit excepturi tempore. Quas, dignissimos incidunt in, nemo alias dolore provident doloremque voluptas, harum sunt consequuntur quo saepe similique ipsum velit officia neque earum.</p>
     </div>
   );
 }
